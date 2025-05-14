bfa0361fd27f61c4b70f4a75499a754a27c47618
wskalka <william.skalka@defra.gov.uk>
2025-05-14 13:31:50 +0100

    Initial Codeset Publish

--- Badger License Applications Part 1/.local/install/CodedWorkflow.cs
+++ Badger License Applications Part 1/.local/install/CodedWorkflow.cs
@@ -21,7 +21,7 @@ namespace BadgerLicenseApplicationsPart1
     {
         public CodedWorkflow()
         {
-            _ = new System.Type[]{typeof(UiPath.Mail.Activities.Api.IMailService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.Testing.API.ITestingService)};
+            _ = new System.Type[]{typeof(UiPath.Testing.API.ITestingService), typeof(UiPath.Core.Activities.API.ISystemService), typeof(UiPath.UIAutomationNext.API.Contracts.IUiAutomationAppService), typeof(UiPath.Mail.Activities.Api.IMailService)};
         }
 
         protected UiPath.Mail.Activities.Api.IMailService mail { get => serviceContainer.Resolve<UiPath.Mail.Activities.Api.IMailService>(); }
